784bb1dcadc818d1d70df4c55ec954845537d04d
gmful <gmfullenkamp@gmail.com>
2023-01-09 20:50:19 -0500

    Updated bash script to support cv2.

--- bash.sh
+++ bash.sh
@@ -2,4 +2,7 @@
 # This will allow you to uninstall any processes that aren't necessary.
 # systemd-analyze blame
 # systemd-analyze critical chain
-# sudo apt purge <name_of_app>
+# sudo apt uninstall <name_of_app>
+
+# Installs necessary for python-opencv
+sudo apt-get install libhdf5-dev libatlas-base-dev libjasper-dev libgtk2.0-dev pkg-config -y
